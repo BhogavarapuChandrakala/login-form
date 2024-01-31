c9ec5080e03f79bf5c9763a376bc6288339bcfa4
unknown <chandrakala.bhogavarapu@sasi.ac.in>
2024-01-31 16:16:38 +0530

    login form

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from './LoginForm'; // Adjust the import path based on your project structure
+
+const App = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default App;
+
+
